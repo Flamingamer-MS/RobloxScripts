23f963e6213165a946f80f7c63bf95c32bd40638
MetroStudios <vengefulspirit408@gmail.com>
2026-03-25 22:37:33 +0000

    Update doorscript.lua

--- doorscript.lua
+++ doorscript.lua
@@ -1,27 +1,21 @@
-local door = script.Parent
+local trigger = script.Parent
+local door = trigger.Parent:FindFirstChild("d1") -- change this if your door part has a different name
 local TweenService = game:GetService("TweenService")
 
-local opened = false
-local debounce = false
+local isOpen = false
 
 local closedCFrame = door.CFrame
-local openCFrame = door.CFrame * CFrame.new(0, 0, -5)
+local openCFrame = closedCFrame * CFrame.new(-4, 0, 0) -- slides sideways relative to door
 
 local tweenInfo = TweenInfo.new(1, Enum.EasingStyle.Sine, Enum.EasingDirection.Out)
 local openTween = TweenService:Create(door, tweenInfo, {CFrame = openCFrame})
 local closeTween = TweenService:Create(door, tweenInfo, {CFrame = closedCFrame})
 
-door.Touched:Connect(function(hit)
-	if debounce or opened then return end
+trigger.Touched:Connect(function(hit)
+	local humanoid = hit.Parent:FindFirstChildOfClass("Humanoid")
+	if humanoid and not isOpen then
+		isOpen = true
 
-	local character = hit.Parent
-	if not character then return end
-
-	local humanoid = character:FindFirstChildOfClass("Humanoid")
-	if humanoid then
-		debounce = true
-		opened = true
-		
 		openTween:Play()
 		openTween.Completed:Wait()
 
@@ -30,7 +24,6 @@ door.Touched:Connect(function(hit)
 		closeTween:Play()
 		closeTween.Completed:Wait()
 
-		opened = false
-		debounce = false
+		isOpen = false
 	end
 end)
